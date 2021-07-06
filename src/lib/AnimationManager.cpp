597cc31e58868036415d3e48c9c5cd9e3fc21fc4
Edison Li <li2343@purdue.edu>
2021-07-06 10:26:29 -0400

    Bug fix and multithreading for AnimationManager.

--- src/lib/AnimationManager.cpp
+++ src/lib/AnimationManager.cpp
@@ -5,23 +5,74 @@
 void UniEngine::AnimationManager::PreUpdate()
 {
     const std::vector<Entity> *owners = EntityManager::GetPrivateComponentOwnersList<Animator>();
-    if (!owners) return;
-    for (auto& i : *owners)
+    if (!owners)
+        return;
+    
+    auto &workers = JobManager::PrimaryWorkers();
+    std::vector<std::shared_future<void>> results;
+    const auto threadSize = workers.Size();
+    const auto threadLoad = owners->size() / threadSize;
+    const auto loadReminder = owners->size() % threadSize;
+    for (int threadIndex = 0; threadIndex < threadSize; threadIndex++)
+    {
+        results.push_back(workers
+                              .Push([=](int id) {
+                                  for (int i = threadIndex * threadLoad; i < (threadIndex + 1) * threadLoad; i++)
+                                  {
+                                      auto &smmc = owners->at(i).GetPrivateComponent<Animator>();
+                                      if (smmc->m_autoPlay)
+                                      {
+                                          smmc->AutoPlay();
+                                          smmc->Animate();
+                                      }else if (
+                                          Application::IsPlaying() && 
+                                          smmc->IsEnabled() &&
+                                          smmc->m_animation && smmc->m_needUpdate)
+                                      {
+                                          smmc->Animate();
+                                          smmc->m_needUpdate = false;
+                                      }
+                                  }
+                                  if (threadIndex < loadReminder)
+                                  {
+                                      const int i = threadIndex + threadSize * threadLoad;
+                                      auto &smmc = owners->at(i).GetPrivateComponent<Animator>();
+                                      if (smmc->m_autoPlay)
+                                      {
+                                          smmc->AutoPlay();
+                                          smmc->Animate();
+                                      }
+                                      else if (
+                                          Application::IsPlaying() && smmc->IsEnabled() && smmc->m_animation &&
+                                          smmc->m_needUpdate)
+                                      {
+                                          smmc->Animate();
+                                          smmc->m_needUpdate = false;
+                                      }
+                                  }
+                              })
+                              .share());
+    }
+    for (const auto &i : results)
+        i.wait();
+    /*
+    for (auto &i : *owners)
     {
         auto &smmc = i.GetPrivateComponent<Animator>();
         if (smmc->m_autoPlay)
         {
             smmc->AutoPlay();
             smmc->Animate();
-            return;
+            continue;
         }
 
         if (smmc->IsEnabled() || !smmc->m_animation)
-            return;
+            continue;
         if (Application::IsPlaying() && smmc->m_needUpdate)
         {
             smmc->Animate();
             smmc->m_needUpdate = false;
         }
     }
+    */
 }
