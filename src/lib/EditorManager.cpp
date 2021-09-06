11d9d84e6a54ac5e5f45abdb212b5da516de8f15
Edison Li <li2343@purdue.edu>
2021-09-06 16:56:03 -0400

    New widget for Editor manager.

--- src/lib/EditorManager.cpp
+++ src/lib/EditorManager.cpp
@@ -1573,3 +1573,54 @@ bool EditorManager::DragAndDropButton(EntityRef &entityRef, const std::string &n
     }
     return statusChanged;
 }
+
+bool EditorManager::DragAndDropButton(
+    AssetRef &target, const std::string &name, const std::vector<std::string> &acceptableTypeNames, bool removable)
+{
+    ImGui::Text(name.c_str());
+    ImGui::SameLine();
+    const std::shared_ptr<IAsset> ptr = target.Get<IAsset>();
+    bool statusChanged = false;
+    ImGui::Button(ptr ? ptr->m_name.c_str() : "none");
+    if (ptr) {
+        if (ImGui::IsItemHovered() && ImGui::IsMouseDoubleClicked(0)) {
+            EditorManager::GetInstance().m_inspectingAsset = ptr;
+        }
+        const std::string tag = "##" + ptr->GetTypeName() + (ptr ? std::to_string(ptr->GetHandle()) : "");
+        if (ImGui::BeginDragDropSource(ImGuiDragDropFlags_SourceAllowNullID)) {
+            ImGui::SetDragDropPayload(ptr->GetTypeName().c_str(), &ptr, sizeof(std::shared_ptr<IAsset>));
+            ImGui::TextColored(ImVec4(0, 0, 1, 1), (ptr->m_name + tag).c_str());
+            ImGui::EndDragDropSource();
+        }
+        if (ImGui::BeginPopupContextItem(tag.c_str())) {
+            if (ImGui::BeginMenu(("Rename" + tag).c_str())) {
+                static char newName[256];
+                ImGui::InputText(("New name" + tag).c_str(), newName, 256);
+                if (ImGui::Button(("Confirm" + tag).c_str()))
+                    ptr->m_name = std::string(newName);
+                ImGui::EndMenu();
+            }
+            if (removable) {
+                if (ImGui::Button(("Remove" + tag).c_str())) {
+                    target.Clear();
+                    statusChanged = true;
+                }
+            }
+            ImGui::EndPopup();
+        }
+    }
+    for(const auto& typeName : acceptableTypeNames) {
+        if (ImGui::BeginDragDropTarget()) {
+            if (const ImGuiPayload *payload = ImGui::AcceptDragDropPayload(typeName.c_str())) {
+                IM_ASSERT(payload->DataSize == sizeof(std::shared_ptr<IAsset>));
+                std::shared_ptr<IAsset> payload_n = *static_cast<std::shared_ptr<IAsset> *>(payload->Data);
+                if (!ptr || payload_n.get() != ptr.get()) {
+                    target = payload_n;
+                    statusChanged = true;
+                }
+            }
+            ImGui::EndDragDropTarget();
+        }
+    }
+    return statusChanged;
+}
